5a21a0603fe802b7510f8f9bd9d667c665993ecf
hyma557 <78861207+hyma557@users.noreply.github.com>
2021-11-05 18:04:57 +0530

    Add files via upload

---
+++ project 97.py
@@ -0,0 +1,6 @@
+guess = input(Guess a number (between 1 - 9))
+if guess == number :
+    print("Congratulations YOU WON!!")
+    
+if not guess < 5:
+    print("YOU LOSS!! TRY HEIGHER THAN", number)
